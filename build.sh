363ed823d17c82a7b5d2775f0c14b9026c3f6c4f
Eric Rasche <rasche.eric@gmail.com>
2016-12-09 18:02:44 +0000

    Build twice just to get the stupid thing working

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+set -ex
+
 cd /apollo/ && \
-	./gradlew :setup-jbrowse && \
-	./apollo clean-all && \
+	./apollo deploy && \
 	cp /apollo/annot.json /apollo/web-app/jbrowse/plugins/WebApollo/json/annot.json && \
+	cp /apollo/annot.json /apollo/jbrowse-download/plugins/WebApollo/json/annot.json && \
+	cp /apollo/annot.json /apollo/client/apollo/json/annot.json && \
 	./apollo deploy

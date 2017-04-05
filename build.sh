1273f914508dc02a8df97e3686400c7b9abbeee9
Eric Rasche <rasche.eric@gmail.com>
2017-04-05 20:20:55 +0000

    fix removal

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ cd /apollo/ && \
 	./apollo deploy && \
 	# Move to tmp dir
 	cp /apollo/target/apollo*.war /tmp/apollo.war && \
-	# So we can remove ~1.6 GB of cruft from the image
-	rm -rf /apollo/ && \
+	# So we can remove ~1.6 GB of cruft from the image. Ignore errors because cannot remove parent dir /apollo/
+	rm -rf /apollo/ || true && \
 	# Before moving back into a standardized location (that we have write access to)
 	mv /tmp/apollo.war /apollo/apollo.war
